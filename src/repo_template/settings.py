69eb47923f8298a22bc5c48afdc74536cb741ccb
Pedro Queiros <coding.paradox013@passmail.net>
2026-03-17 10:57:12 +0100

    updates settings

--- src/repo_template/settings.py
+++ src/repo_template/settings.py
@@ -1,13 +1,22 @@
-from importlib.metadata import version
 import os
+import tomllib
+from importlib.metadata import version
 
-SERVICE_NAME = 'repo_template'
-DEBUG = int(os.environ.get('DEBUG', '0'))
-CODE_VERSION = version(SERVICE_NAME)
-
-APP = os.path.dirname(__file__)
 ROOT = os.path.abspath(os.path.dirname(os.path.dirname(os.path.dirname(__file__))))
+PYPROJECT_PATH = os.path.join(ROOT, 'pyproject.toml')
+
+
+with open(PYPROJECT_PATH, 'rb') as f:
+    data = tomllib.load(f)
+    SERVICE_NAME = data['project']['name']
+
+DEBUG = int(os.getenv('DEBUG', '0'))
+
+
+
 DATA = os.path.join(ROOT, 'data')
 TEMP = os.path.join(ROOT, 'tmp')
 TESTS = os.path.join(ROOT, 'tests')
-CONFIGS = os.path.join(ROOT, 'confs')
+TESTS_DATA = os.path.join(TESTS, 'data')
+CONFIG = os.path.join(ROOT, 'config')
+CODE_VERSION = version(SERVICE_NAME)
